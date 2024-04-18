ac2eecb93d8f58cab73964aadd60852698b4a5ae
shubhammahawar <shubham.mahawar1001@gmail.com>
2024-04-18 17:09:14 +0530

    Update Q14. Kth Largest Element.java

--- Microsoft/Q14. Kth Largest Element.java
+++ Microsoft/Q14. Kth Largest Element.java
@@ -5,6 +5,27 @@ https://www.geeksforgeeks.org/problems/k-largest-elements3736/1
 T.C = O(N*logK)
 S.C = O(k)
 
+
+
+Explanation :
+
+
+Consider the array a = {3, 7, 2, 9, 1} and k = 3.
+
+Initial State: PriorityQueue is empty.
+
+Inserting Elements:
+
+Insert 3: PriorityQueue = {3}
+Insert 7: PriorityQueue = {3, 7}
+Insert 2: PriorityQueue = {2, 3, 7}
+Insert 9: PriorityQueue = {2, 3, 7, 9} (Size > k, remove smallest element)
+Insert 1: PriorityQueue = {1, 2, 3, 7, 9} (Size > k, remove smallest element)
+Result:
+
+After inserting all elements, the PriorityQueue contains the elements {1, 2, 3, 7, 9}.
+Since k = 3, we only need the top k elements from the PriorityQueue, which are {7, 9, 3}.
+
 // Java code for k largest elements in an array
 
 
